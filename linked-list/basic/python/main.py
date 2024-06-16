6fba73b95a2f7787d1577235bafbc33f8984aeaa
Yuzuru Nishimiya <sonyalamsyah.learn@gmail.com>
2024-06-16 09:32:49 +0700

    LL:basic:py: refactor code

--- linked-list/basic/python/main.py
+++ linked-list/basic/python/main.py
@@ -1,40 +1,78 @@
-from typing import Any
+"""
+Linked List:
+    linear data structure that include a series of connected nodes.
+    Head -> [data|next] -> [data|next] -> Null
+    types:
+        - singly
+        - doubly
+        - circular
+    Application:
+        - Dynamic memory allocation
+        - Implemented in stack and queue
+        - In Undo function of sofwares
+        - Hash tables, graphs
+
+    This One Below is singly
+"""
+
+from typing import Any, Optional
 
 
 class Node:
-    def __init__(self, item) -> None:
-        self.__item = item
-        self.__next = None
-    
+    """Node's class"""
+
+    def __init__(self, data: Optional[Any] = None) -> None:
+        self.__data: Any = data
+        self.__next: __class__ = None
+
     @property
     def next(self):
+        """get next node"""
+
         return self.__next
-    
+
     @next.setter
     def next(self, value: Any):
+        """set next node"""
+
         self.__next = value
 
     @property
-    def item(self):
-        return self.__item
+    def data(self) -> Any:
+        """get data"""
+
+        return self.__data
+
+    @data.setter
+    def data(self, val: Any) -> None:
+        """set data"""
+
+        self.__data = val
+
 
 class LinkedList:
-    def __init__(self) -> None:
-        self.__head = None
+    """Linked List's class"""
+
+    def __init__(self, head: Optional[Node] = None) -> None:
+        self.__head = head
 
     @property
     def head(self):
+        """get head"""
+
         return self.__head
-    
+
     @head.setter
     def head(self, value: Node):
+        """set nead"""
+
         self.__head = value
 
 
 if __name__ == '__main__':
 
     linked_list = LinkedList()
-    
+
     # assign item value
     linked_list.head = Node(1)
     second = Node(2)
@@ -46,6 +84,6 @@ if __name__ == '__main__':
 
     # print
     next_nodes: Node = linked_list.head
-    while next_nodes != None:
-        print(next_nodes.item, end=" ")
+    while next_nodes is not None:
+        print(next_nodes.data, end=" ")
         next_nodes = next_nodes.next
