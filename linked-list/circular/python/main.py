645a47fd4ac7dcb23e24a5634854973d3802dc7d
Yuzuru Nishimiya <sonyalamsyah.learn@gmail.com>
2024-06-17 09:01:41 +0700

    linked-list:circular:py refactor code

--- linked-list/circular/python/main.py
+++ linked-list/circular/python/main.py
@@ -9,6 +9,9 @@
 
 """
 
+from typing import Any
+
+
 class Node:
     """Node Class [Sebuah konstruksi node]"""
 
@@ -21,21 +24,33 @@ class Node:
     @property
     def next_node(self):
         """get next node data"""
+
         return self.__next
 
     @next_node.setter
     def next_node(self, node):
         """a method to set a next node"""
+
         self.__next = node
 
     @property
     def data(self):
         """get data"""
+
         return self.__data
+
+    @data.setter
+    def data(self, value: Any):
+        """set data"""
+
+        # you can add a logic or validation before assign the value
+        # below here
+        self.__data = value
 
 
 class CircularLinkedList:
     """Circular Linked List Class [Konstruksi yang mengatur linked list]"""
+
     def __init__(self, head: Node = None) -> None:
         self.__head = head
 
@@ -49,18 +64,79 @@ class CircularLinkedList:
         """to set head (a node class) to the circular linked list"""
         self.__head = head
 
+    def insert_at_beginning(self, new_data: Any) -> None:
+        """to insert at beginning of LL"""
+
+        new_node = Node(new_data)
+
+        if self.__head is None:
+            pass
+        elif self.__head.next_node is None:
+            self.__head.next_node = new_node
+            new_node.next_node = self.__head
+        else:
+            last_node = self.__head
+            while last_node.next_node is not self.__head:
+                last_node = last_node.next_node
+
+            last_node.next_node = new_node
+            new_node.next_node = self.__head
+
+        self.__head = new_node
+
+    def insert_after(self, previous_node: Node, new_node_data: Any) -> None:
+        """to create new node + insert new data to next node"""
+
+        new_node = Node(new_node_data)
+        if self.__head is None:
+            self.__head = new_node
+        elif previous_node is self.__head:
+            self.__head.next_node = new_node
+            new_node.next_node = self.__head
+        else:
+            new_node.next_node = previous_node.next_node
+            previous_node.next_node = new_node
+
+    def insert_at_end(self, new_node_data: Any) -> None:
+        """to creat new node + insert node in the end of node"""
+
+        new_node = Node(new_node_data)
+        if self.__head is None:
+            self.__head = new_node
+        else:
+            last_node = self.__head
+            while last_node and last_node.next_node is not self.__head:
+                last_node = last_node.next_node
+
+            last_node.next_node = new_node
+            new_node.next_node = self.__head
+
+    def print_list(self) -> None:
+        """print linked list"""
+
+        node = self.__head
+
+        while node:
+            print(f"{node.data}", end=", ")
+            node: Node = node.next_node
+            if node is self.__head:
+                print("reached the last node a.k.a head of LL, val: ", node.data, end=" ")
+                break
+        else:
+            print("next-node is null (head only)", end=".")
+
+        print() # to the grand line
+
 
 if __name__ == "__main__":
-    # set nodes
-    one = Node(1)
-    two = Node(2)
-    three = Node(3)
-
-    # connect nodes circular
-    one.next_node = two
-    two.next_node = three
-    three.next_node = one
-
-    # set head
-    circular_linked_list = CircularLinkedList()
-    circular_linked_list.head = one
+    print(" --- start --- ")
+    ll = CircularLinkedList()
+    ll.insert_at_beginning(10)
+    ll.print_list()
+    ll.insert_at_beginning(9)
+    ll.print_list()
+    ll.insert_at_end(11)
+    ll.print_list()
+    ll.insert_at_beginning(0)
+    ll.print_list()
+    print(" --- end --- ")
